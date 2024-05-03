ce0b5de0db919e6db45dac38abe19f22bf404e3f
lszgege <lszgege@163.com>
2024-05-03 21:50:02 +0800

    init v1

---
+++ batchWallet/batchWallet/src/main/java/com/example/batchwallet/BatchWalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchWalletApplication.class, args);
+	}
+
+}
---
+++ batchWallet/batchWallet/src/test/java/com/example/batchwallet/BatchWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
